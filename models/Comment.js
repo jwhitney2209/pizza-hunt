5c830d460e578bc6ca3ae4e9c7df393a3191a8c2
teachergg <jwhitney@bisd.us>
2022-06-01 03:21:46 -0500

    added validation

--- models/Comment.js
+++ models/Comment.js
@@ -9,10 +9,14 @@ const ReplySchema = new Schema(
       default: () => new Types.ObjectId()
     },
     replyBody: {
-      type: String
+      type: String,
+      required: 'You need to provide a reply!',
+      trim: true
     },
     writtenBy: {
-      type: String
+      type: String,
+      required: 'You need to provide a name!',
+      trim: true
     },
     createdAt: {
       type: Date,
@@ -30,10 +34,14 @@ const ReplySchema = new Schema(
 const CommentSchema = new Schema(
   {
     writtenBy: {
-      type: String
+      type: String,
+      required: 'You need to provide a name!',
+      trim: true
     },
     commentBody: {
-      type: String
+      type: String,
+      required: 'You need to provide a comment!',
+      trim: true
     },
     createdAt: {
       type: Date,
